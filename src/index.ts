ef3ec5fc7a2eaf4f4a9aa02bcd8807d8fe6b4f4f
EtshD1 <H79Pain@Gmail.com>
2021-04-24 13:55:36 +0200

    add claims history Reducer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { createStore, combineReducers } from '@reduxjs/toolkit';
 import types from "./types";
 
+// Action Creators
 const createClaim = (name: string, amount: number): object => (
   {
     type: types.CREATE_CLAIM,
@@ -25,3 +26,11 @@ const deletePolicy = (name: string): object => ({
     name: name
   }
 });
+
+// Reducers
+const claimsHistory = (history: Array<object>, action: { type: string, payload: object }): Array<object> => {
+  if (action.type === types.CREATE_CLAIM) {
+    return [...history, action.payload];
+  }
+  return history
+};
