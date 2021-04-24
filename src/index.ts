588866f4dbee0d29ccad39776627914741ca8251
EtshD1 <H79Pain@Gmail.com>
2021-04-24 14:56:57 +0200

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { createStore, combineReducers } from '@reduxjs/toolkit';
 import types from "./types";
 
-interface Action {
+interface InsuranceActions {
   type: string;
   payload: any
 }
 // Action Creators
-const createClaim = (name: string, amount: number): object => {
+const createClaim = (name: string, amount: number): InsuranceActions => {
   if (amount > 40) {
     return {
       type: types.CREATE_CLAIM,
@@ -25,30 +25,30 @@ const createClaim = (name: string, amount: number): object => {
   }
 };
 
-const createPolicy = (name: string): object => ({
+const createPolicy = (name: string): InsuranceActions => ({
   type: types.CREATE_POLICY,
-  paylord: {
+  payload: {
     name: name,
     amount: 20
   }
 });
 
-const deletePolicy = (name: string): object => ({
+const deletePolicy = (name: string): InsuranceActions => ({
   type: types.CREATE_POLICY,
-  paylord: {
+  payload: {
     name: name
   }
 });
 
 // Reducers
-const claimsHistory = (history: Array<object> = [], action: Action): Array<object> => {
+const claimsHistory = (history: Array<object> = [], action: InsuranceActions): Array<object> => {
   if (action.type === types.CREATE_CLAIM) {
     return [...history, action.payload];
   }
   return history
 };
 
-const accounting = (wallet: number = 100, action: Action): number => {
+const accounting = (wallet: number = 100, action: InsuranceActions): number => {
   if (action.type === types.CREATE_CLAIM) {
     return wallet - action.payload.amount;
   } else if (action.type === types.CREATE_POLICY) {
@@ -57,7 +57,7 @@ const accounting = (wallet: number = 100, action: Action): number => {
   return wallet;
 }
 
-const policies = (policyList: Array<object> = [], action: Action): Array<object> => {
+const policies = (policyList: Array<object> = [], action: InsuranceActions): Array<object> => {
   if (action.type === types.CREATE_POLICY) {
     return [...policyList, action.payload.name];
   } else if (action.type === types.DELTE_POLICY) {
@@ -73,3 +73,7 @@ const reducers = combineReducers({
   claimsHistory,
   policies
 });
+
+const store = createStore(reducers);
+
+store.dispatch(createPolicy('Alex'));
