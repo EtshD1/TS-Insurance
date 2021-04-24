3837f123166469af89aa6955827947a43004c830
EtshD1 <H79Pain@Gmail.com>
2021-04-24 13:26:42 +0200

    add create claim action creator

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("IT'S ALIVE")
+import { createStore, combineReducers } from '@reduxjs/toolkit';
+import types from "./types";
+
+const createClaim = (name: string, amount: number): object => (
+  {
+    type: types.CREATE_CLAIM,
+    payload: {
+      name,
+      amount
+    }
+  }
+);
